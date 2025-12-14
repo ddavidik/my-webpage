aa2a78304ffbce3968b6a25600c75ee45d99f331
Daniel Davidík <d.davidik7@gmail.com>
2025-12-14 15:24:09 +0100

    build: :building_construction: Migrate to Bun

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+import netlify from '@netlify/vite-plugin-tanstack-start';
 
 const config = defineConfig({
   plugins: [
@@ -16,11 +16,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    viteReact({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
+    viteReact(),
   ],
 })
 
